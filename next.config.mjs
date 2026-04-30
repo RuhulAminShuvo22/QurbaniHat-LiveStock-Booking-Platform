ba742599ef2d8495347ff5dc70bf2870b55a6cfe
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-30 17:21:45 +0600

    update and currection on next.config.mjs section

--- next.config.mjs
+++ next.config.mjs
@@ -27,42 +27,84 @@
 // };
 
 // export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactCompiler: true,
+
+//   images: {
+//     // multiple host allow করা
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'plus.unsplash.com', // কখনো কখনো unsplash এইটা use করে
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'cdn.pixabay.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'i.ibb.co',
+//       },
+//     ],
+
+//     // optional fallback (simple way)
+//     domains: [
+//       'images.unsplash.com',
+//       'plus.unsplash.com',
+//       'cdn.pixabay.com',
+//       'i.ibb.co',
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
 
   images: {
-    // multiple host allow করা
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com', // কখনো কখনো unsplash এইটা use করে
+        protocol: "https",
+        hostname: "plus.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'cdn.pixabay.com',
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
       },
+
+      // তোমার JSON data এর domain
       {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
+        protocol: "https",
+        hostname: "i.ibb.co.com",
+      },
+
+      // যদি i.ibb.co use করো future এ
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
       },
     ],
 
-    // optional fallback (simple way)
     domains: [
-      'images.unsplash.com',
-      'plus.unsplash.com',
-      'cdn.pixabay.com',
-      'i.ibb.co',
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "cdn.pixabay.com",
+      "i.ibb.co.com",
+      "i.ibb.co",
     ],
   },
 };
 
 export default nextConfig;
-
-
 
