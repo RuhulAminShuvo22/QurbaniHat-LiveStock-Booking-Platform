12b2f2fffb040fa5caea642ba53d35d73e541d67
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-05-01 10:56:27 +0600

    mongo bd related work

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,19 +1,35 @@
-import dns from "node:dns";
-dns.setServers(['8.8.8.8' , '8.8.4.4']);
+// import dns from "node:dns";
+// dns.setServers(['8.8.8.8' , '8.8.4.4']);
+
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+// const client = new MongoClient("mongodb://localhost:27017/database");
+// const db = client.db(process.env.MONGODB_URI);
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(db, {
+//     // Optional: if you don't provide a client, database transactions won't be enabled.
+//     client
+//   }),
+//   emailAndPassword: { 
+//     enabled: true, 
+//   },
+// });
 
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
-const db = client.db(process.env.MONGODB_URI);
+
+const client = new MongoClient(process.env.MONGO_URI); // connection string pabo mangodb thrkr
+const db = client.db();
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
-  emailAndPassword: { 
-    enabled: true, 
-  },
 });
+//qurbanirhat
