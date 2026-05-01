4ee580130a8badab001d1be5f7637ab5de409e18
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-05-01 20:51:17 +0600

    in top animal section little change

--- src/components/TopAnimals.jsx
+++ src/components/TopAnimals.jsx
@@ -3,7 +3,7 @@ import AnimalCard from "./AnimalCard";
 
 const TopAnimals = async () => {
 
-    const res = await fetch("https://qurbani-hat-live-stock-booking-plat.vercel.app/data.json")
+    const res = await fetch("http://localhost:3000/data.json")
     const animals = await res.json()
     //console.log(animals,"animals")
     const topAnimals = animals.slice(0, 8);
@@ -28,3 +28,4 @@ const TopAnimals = async () => {
 };
 
 export default TopAnimals;
+
