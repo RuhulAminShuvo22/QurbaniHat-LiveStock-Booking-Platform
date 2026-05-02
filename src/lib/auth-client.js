4f8328e29a294bb2cea94c5542446ccbba046513
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-05-02 13:22:40 +0600

    update on login errors

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,21 @@
+// import { createAuthClient } from "better-auth/react"
+// export const authClient = createAuthClient({
+//     /** The base URL of the server (optional if you're using the same domain) */
+//     baseURL: "http://localhost:3000"  // ei link change hobe jokhon finally submit korbo
+// })
+
+// export const { signIn, signUp, useSession } = createAuthClient()
+
 import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"  // ei link change hobe jokhon finally submit korbo
+    /** 
+     * baseURL: প্রোডাকশনে আপনার ভার্সেল লিঙ্ক কাজ করবে 
+     * এবং লোকালে ৩০০০ পোর্ট কাজ করবে।
+     */
+    baseURL: process.env.NODE_ENV === "production" 
+        ? "https://qurbani-hat-live-stock-booking-plat.vercel.app" 
+        : "http://localhost:3000"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
